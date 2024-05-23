a1b7955c47448942e4ffa2048e0c4cb283e127e3
Jefferson Hernandez <JeffersonH0501>
2024-05-23 09:35:15 -0500

    commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+
+import { AppController } from './app.controller';
+
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+      TypeOrmModule.forRoot(databaseConfig),
+      /*TypeOrmModule.forFeature([AlbumEntity])*/
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ]
+})
+
+export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'jefferson',
+    password: '123',
+    database: 'parcial',
+    synchronize: true,
+    autoLoadEntities: true,
+};
